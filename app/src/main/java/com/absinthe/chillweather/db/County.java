c48be9ff0cea5f450077322c2b99226308e9af77
zhaobozhen <zhaobozhen2025@gmail.com>
2018-10-15 14:40:42 +0800

    add configs of creating dbs and tables.

---
+++ app/src/main/java/com/absinthe/chillweather/db/County.java
@@ -0,0 +1,42 @@
+package com.absinthe.chillweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;
+    private int weathertId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeathertId() {
+        return weathertId;
+    }
+
+    public void setWeathertId(int weathertId) {
+        this.weathertId = weathertId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/absinthe/chillweather/db/Province.java
@@ -0,0 +1,33 @@
+package com.absinthe.chillweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
